07f7cb5c886b5cd21e4f25c7a190898504d28dcc
Angel Jesus <230592@utxicotepec.edu.mx>
2024-11-06 16:26:49 -0600

    practica11
    creando rutas para las paginas principales de BienesRaices

---
+++ controllers/usuarioController.js
@@ -0,0 +1,24 @@
+
+const formularioLogin = (req, res) => {
+    res.render('auth/login',{
+        pagina: 'Iniciar Sesion'
+    })
+}
+
+const formularioRegistro = (req, res) => {
+    res.render('auth/registro',{
+        pagina: 'Crear Cuenta'
+    })
+}
+
+const formularioOlvidePassword = (req, res) => {
+    res.render('auth/Olvide-Password',{
+        pagina: 'Recupera tu Cuenta por Contraseña Perdida'
+    })
+}
+
+export{
+    formularioLogin,
+    formularioRegistro,
+    formularioOlvidePassword
+}
