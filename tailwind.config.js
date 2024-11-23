b5bd66f494ad51503026fd0d5989b9e18d40ac5c
Angel Jesus <230592@utxicotepec.edu.mx>
2024-11-23 14:15:01 -0600

    Examen Practico
    Se concluye el merge de la rama de examen practico con la estilizacion del login y agregacion del campo de fecha de nacimiento

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,27 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        angelPaleta: {
+          'custom-green': '#91CB3E',
+          'custom-green-hover': '#53A548',
+          'custom-black-hover': '#00000',
+          'custom-sea-green': '#4C934C',
+          white: '#FFFFFF',
+          black: '#000000',
+          'light-gray': '#f7f7f7',
+          'dark-gray': '#333333',
+        },
+      },
+      backgroundImage: {
+        'real-estate': 'linear-gradient(to right, #1e3c72, #2a5298)', // Azul sofisticado
+        'warm-sunset': 'linear-gradient(to bottom, #ff7e5f, #feb47b)', // Atardecer cálido
+        'green-growth': 'linear-gradient(to right, #56ab2f, #a8e063)', // Verde
+        'red-adv': 'linear-gradient(to right, #ff0000, #f16446)', // Rojo
+      },
       animation: {
-        'fade-in': 'fadeIn 2s ease-out',
-        'fade-out': 'fadeOut 15s ease-in-out forwards',
+        'fade-in': 'fadeIn 1s ease-in-out forwards',
+        'fade-out': 'fadeOut 15s ease-in-out forwards', // Ajustado para 3 segundos
       },
       keyframes: {
         fadeIn: {
@@ -17,23 +35,10 @@ module.exports = {
         },
         fadeOut: {
           '0%': { opacity: 1 },
-          '100%': { opacity: 0, display: 'none' },
+          '100%': { opacity: 0, display: 'none' }, // Continuamos con display: none
         },
-      },
-      colors: {
-        angelPaleta: {
-          'custom-green': '#91CB3E',
-          'custom-green-hover': '#53A548',
-          'custom-sea-green': '#4C934C',
-        },
-      },
-      backgroundImage: {
-        'real-estate': 'linear-gradient(to right, #1e3c72, #2a5298)', // Azul sofisticado
-        'warm-sunset': 'linear-gradient(to bottom, #ff7e5f, #feb47b)', // Atardecer cálido
-        'green-growth': 'linear-gradient(to right, #56ab2f, #a8e063)', // Verde
-        'red-adv': 'linear-gradient(to right,   #ff0000 ,   #f16446   )', //rojo
       },
     },
   },
   plugins: [],
-};
+}
